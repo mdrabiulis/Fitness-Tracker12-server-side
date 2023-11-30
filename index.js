e204c31cebc996cbf453902445ccc8a4a74e2f10
mdrabiulis <mdrabiulis155@gmail.com>
2023-11-30 23:24:41 +0600

    Newsletter post data

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const access = process.env;
 
-
-
 // Express middleware
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
 
 const uri = `mongodb+srv://${access.USER_AC}:${access.DATABASE_SECRET_KEY}@cluster0.wvvqbv9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,47 +19,38 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
+    const Newslettercollection = client
+      .db("assignment12DB")
+      .collection("newsletter");
 
-
-
-
-
-
-
-
-
-
-
-
+    // Newsletter
+    app.post("/newsletter", async (req, res) => {
+      const data = req.body;
+      const result = await Newslettercollection.insertOne(data);
+      res.send(result);
+    });
 
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Fitness Tracker Website");
