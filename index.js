6ac004a52d6d808ec49fb41158e50c4b56e610a1
mdrabiulis <mdrabiulis155@gmail.com>
2023-12-01 00:33:08 +0600

    Newsletter get  data

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/newsletter", async (req, res) => {
+      const result = await Newslettercollection.find().toArray();
+      res.send(result);
+    });
 
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
